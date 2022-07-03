9ab10415ef145eba5ff1c7712e3d5f77fd39e430
Natesh Narain <nnaraindev@gmail.com>
2022-07-03 02:04:54 -0400

    Embassy for embedded async support

--- icarus/src/lib.rs
+++ icarus/src/lib.rs
@@ -62,61 +62,65 @@ pub struct Icarus {
 }
 
 impl Icarus {
+    pub fn init(p: Peripherals) -> Result<Icarus, IcarusError> {
+        let mut system = p.SYSTEM.split();
+        let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+        // Disable watchdog timers
+        let mut rtc_cntl = RtcCntl::new(p.RTC_CNTL);
+        let mut timer0 = Timer::new(p.TIMG0);
+        let mut timer1 = Timer::new(p.TIMG1);
+
+        rtc_cntl.set_super_wdt_enable(false);
+        rtc_cntl.set_wdt_enable(false);
+        timer0.disable();
+        timer1.disable();
+
+        let io = IO::new(p.GPIO, p.IO_MUX);
+
+        let drv2_en = io.pins.gpio6.into_push_pull_output();
+        let rtrctl2 = io.pins.gpio7.into_push_pull_output();
+        let rtrctl1 = io.pins.gpio8.into_push_pull_output();
+
+        let drv1_en = io.pins.gpio10.into_push_pull_output();
+        let rtrctl4 = io.pins.gpio4.into_push_pull_output();
+        let rtrctl3 = io.pins.gpio5.into_push_pull_output();
+
+        let user_btn = io.pins.gpio9.into_floating_input();
+
+        let pulse = PulseControl::new(
+            p.RMT,
+            &mut system.peripheral_clock_control,
+            ClockSource::APB,
+            0,
+            0,
+            0,
+        )
+        .map_err(|_| IcarusError::HardwareInitError)?;
+
+        let stat = <smartLedAdapter!(1)>::new(pulse.channel0, io.pins.gpio21);
+
+        let battery_sense = io.pins.gpio3.into_push_pull_output();
+
+        let delay = Delay::new(&clocks);
+
+        Ok(Icarus {
+            drv1_en,
+            drv2_en,
+            rtrctl1,
+            rtrctl2,
+            rtrctl3,
+            rtrctl4,
+            user_btn,
+            stat,
+            battery_sense,
+            delay,
+        })
+    }
+
     pub fn take() -> Result<Icarus, IcarusError> {
-        if let Some(dp) = Peripherals::take() {
-            let mut system = dp.SYSTEM.split();
-            let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
-
-            // Disable watchdog timers
-            let mut rtc_cntl = RtcCntl::new(dp.RTC_CNTL);
-            let mut timer0 = Timer::new(dp.TIMG0);
-            let mut timer1 = Timer::new(dp.TIMG1);
-
-            rtc_cntl.set_super_wdt_enable(false);
-            rtc_cntl.set_wdt_enable(false);
-            timer0.disable();
-            timer1.disable();
-
-            let io = IO::new(dp.GPIO, dp.IO_MUX);
-
-            let drv2_en = io.pins.gpio6.into_push_pull_output();
-            let rtrctl2 = io.pins.gpio7.into_push_pull_output();
-            let rtrctl1 = io.pins.gpio8.into_push_pull_output();
-
-            let drv1_en = io.pins.gpio10.into_push_pull_output();
-            let rtrctl4 = io.pins.gpio4.into_push_pull_output();
-            let rtrctl3 = io.pins.gpio5.into_push_pull_output();
-
-            let user_btn = io.pins.gpio9.into_floating_input();
-
-            let pulse = PulseControl::new(
-                dp.RMT,
-                &mut system.peripheral_clock_control,
-                ClockSource::APB,
-                0,
-                0,
-                0,
-            )
-            .map_err(|_| IcarusError::HardwareInitError)?;
-
-            let stat = <smartLedAdapter!(1)>::new(pulse.channel0, io.pins.gpio21);
-
-            let battery_sense = io.pins.gpio3.into_push_pull_output();
-
-            let delay = Delay::new(&clocks);
-
-            Ok(Icarus {
-                drv1_en,
-                drv2_en,
-                rtrctl1,
-                rtrctl2,
-                rtrctl3,
-                rtrctl4,
-                user_btn,
-                stat,
-                battery_sense,
-                delay,
-            })
+        if let Some(p) = Peripherals::take() {
+            Icarus::init(p)
         } else {
             Err(IcarusError::HardwareInitError)
         }
