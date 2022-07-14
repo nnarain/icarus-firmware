40f6aceb15e15a42301b2e9dab0ffb43b8215ead
Natesh Narain <nnaraindev@gmail.com>
2022-07-14 18:41:51 -0400

    Resolves #22. Simple two way communication between host and controller over USB Serial/JTAG

--- icarus-app-std/src/main.rs
+++ icarus-app-std/src/main.rs
@@ -5,7 +5,8 @@
 // @date Jul 10 2022
 //
 
-use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+use icarus_app_std::stat::{StatLed, StatColor};
+use icarus_wire::{self, IcarusState, IcarusCommand, ImuRaw};
 
 use esp_idf_hal::{
     prelude::*,
@@ -14,13 +15,21 @@ use esp_idf_hal::{
     i2c,
     delay::FreeRtos,
 };
-use icarus_app_std::stat::{StatLed, StatColor};
 
-use embedded_hal::i2c::blocking::I2c;
+use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
-use mpu6050::{Mpu6050, Mpu6050Error};
 
-use std::{borrow::Borrow, sync::Arc, time::Duration, thread};
+use mpu6050::Mpu6050;
+
+use std::{
+    sync::{
+        Arc,
+        mpsc::channel,
+    },
+    time::Duration,
+    thread,
+    io::{Write, Read}
+};
 
 #[allow(unreachable_code)]
 fn main() -> anyhow::Result<()> {
@@ -28,26 +37,32 @@ fn main() -> anyhow::Result<()> {
     // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
     esp_idf_sys::link_patches();
 
-    let mut p = Peripherals::take().unwrap();
+    // -----------------------------------------------------------------------------------------------------------------
+    // Setup Logging
+    // -----------------------------------------------------------------------------------------------------------------
+
+    // TODO(nnarain): This library depends on cortex-m crate
+    // let mut logger = defmt_bbq::init().unwrap();
 
     // -----------------------------------------------------------------------------------------------------------------
     // Hardware Init
     // -----------------------------------------------------------------------------------------------------------------
+    let mut p = Peripherals::take().unwrap();
 
     // Rotor control
-    let drv1_en = p.pins.gpio10.into_output()?;
-    let drv2_en = p.pins.gpio6.into_output()?;
+    let _drv1_en = p.pins.gpio10.into_output()?;
+    let _drv2_en = p.pins.gpio6.into_output()?;
 
     let config = config::TimerConfig::default().frequency(50.Hz().into());
     let timer = Arc::new(Timer::new(p.ledc.timer0, &config)?);
 
-    let rtrctl1 = Channel::new(p.ledc.channel0, timer.clone(), p.pins.gpio8)?;
-    let rtrctl2 = Channel::new(p.ledc.channel1, timer.clone(), p.pins.gpio7)?;
-    let rtrctl3 = Channel::new(p.ledc.channel2, timer.clone(), p.pins.gpio5)?;
-    let rtrctl4 = Channel::new(p.ledc.channel3, timer.clone(), p.pins.gpio4)?;
+    let _rtrctl1 = Channel::new(p.ledc.channel0, timer.clone(), p.pins.gpio8)?;
+    let _rtrctl2 = Channel::new(p.ledc.channel1, timer.clone(), p.pins.gpio7)?;
+    let _rtrctl3 = Channel::new(p.ledc.channel2, timer.clone(), p.pins.gpio5)?;
+    let _rtrctl4 = Channel::new(p.ledc.channel3, timer.clone(), p.pins.gpio4)?;
 
     // GPIO
-    let user_button = p.pins.gpio9.into_input()?;
+    let _user_button = p.pins.gpio9.into_input()?;
 
     // Stat LED
     let mut stat_led = StatLed::new(p.pins.gpio21, p.rmt.channel0)?;
@@ -70,23 +85,120 @@ fn main() -> anyhow::Result<()> {
     // Tasks
     // -----------------------------------------------------------------------------------------------------------------
 
-    // TODO...
+    // Setup task queues
+    let (cmd_sender, cmd_reciever) = channel::<IcarusCommand>();
 
-    let colors: [StatColor; 3] = [StatColor::Red, StatColor::Green, StatColor::Blue];
-    let mut c = 0;
+    let (state_sender, state_reciever) = channel::<IcarusState>();
+    let imu_state_sender = state_sender.clone();
+
+    let (led_sender, led_reciever) = channel::<IcarusCommand>();
+
+    // Spawn command task
+    thread::spawn(move || {
+        let mut read_buf: [u8; 64] = [0; 64];
+        loop {
+            match std::io::stdin().read(&mut read_buf) {
+                Ok(n) => {
+                    let mut remaining = Some(&mut read_buf[..n]);
+
+                    loop {
+                        remaining = if let Some(bytes) = remaining {
+                            match icarus_wire::decode::<IcarusCommand>(bytes) {
+                                Ok((cmd, unused)) => {
+                                    cmd_sender.send(cmd).expect("Failed to send recieved command on channel");
+                                    Some(unused)
+                                },
+                                Err(_) => None,
+                            }
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                },
+                Err(_) => {},
+            }
+            thread::sleep(Duration::from_millis(10));
+        }
+    });
+
+    // Spawn sensors task
+    thread::spawn(move || {
+        loop {
+            let accel = imu.get_acc();
+            let gyro = imu.get_gyro();
+            let temp = imu.get_temp();
+
+            if let (Ok(accel), Ok(gyro), Ok(temp)) = (accel, gyro, temp) {
+                // TODO: Actual state estimation
+                let imu_raw = ImuRaw {
+                    accel: (accel.x, accel.y, accel.z),
+                    gyro: (gyro.x, gyro.y, gyro.z),
+                    temp,
+                };
+                let imu_raw = IcarusState::ImuRaw(imu_raw);
+
+                imu_state_sender.send(imu_raw).expect("Failed to send IMU data");
+            }
+
+            thread::sleep(Duration::from_millis(1000));
+        }
+    });
+
+    // Spawn LED task
+    thread::spawn(move || {
+        let colors: [StatColor; 3] = [StatColor::Red, StatColor::Green, StatColor::Blue];
+        let mut c = 0;
+
+        loop {
+            // stat_led.update(colors[c]).unwrap();
+            // c = (c + 1) % colors.len();
+
+            // thread::sleep(Duration::from_millis(1000));
+            match led_reciever.recv() {
+                Ok(cmd) => {
+                    stat_led.update(colors[c]).unwrap();
+                    c = (c + 1) % colors.len();
+                },
+                Err(_) => {}
+            }
+        }
+    });
+
+    let mut send_buf: [u8; 64] = [0; 64];
 
     // Idle Task
     loop {
-        let accel_data = imu.get_acc().unwrap();
-        let gyro_data = imu.get_gyro().unwrap();
+        // Write all recieved state to serial port
+        loop {
+            match state_reciever.try_recv() {
+                Ok(state) => {
+                    let used_buf = icarus_wire::encode(&state, &mut send_buf)?;
+                    std::io::stdout().write_all(&used_buf)?;
 
-        println!("A: {:?}, G: {:?}", accel_data, gyro_data);
+                    std::io::stdout().flush()?;
+                    // TODO(nnarain): Why is this needed?
+                    println!("\n\r");
+                },
+                Err(_) => {
+                    break;
+                },
+            }
+        }
 
-        stat_led.update(colors[c])?;
-        c = (c + 1) % colors.len();
+        // Recieve commands and dispatch to tasks
+        loop {
+            match cmd_reciever.try_recv() {
+                Ok(cmd) => {
+                    match cmd {
+                        IcarusCommand::CycleLed => led_sender.send(cmd.clone()).unwrap(),
+                    }
+                },
+                Err(_) => break,
+            }
+        }
 
-        // TODO(nnarain): Write state updates to serial port
-        thread::sleep(Duration::from_millis(1000u64));
+        thread::sleep(Duration::from_millis(10));
     }
 
     Ok(())
---
+++ icarus-wire/src/lib.rs
@@ -0,0 +1,36 @@
+#![no_std]
+use serde::{Serialize, Deserialize};
+
+// Re-export postcard functions for encoding and decoding
+pub use postcard::{
+    to_slice_cobs as encode,
+    take_from_bytes_cobs as decode,
+    to_vec_cobs as decode_vec,
+};
+
+pub use postcard::{Result, Error};
+
+/// Raw data from the IMU
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+pub struct ImuRaw {
+    pub accel: (f32, f32, f32),
+    pub gyro: (f32, f32, f32),
+    pub temp: f32,
+}
+
+/// IMU calibration offset
+pub struct ImuCalibrationOffset {
+
+}
+/// Data reporting channels for Icarus
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+pub enum IcarusState<'a> {
+    Log(&'a [u8]),
+    ImuRaw(ImuRaw),
+}
+
+/// Icarus command channels
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+pub enum IcarusCommand {
+    CycleLed,
+}
