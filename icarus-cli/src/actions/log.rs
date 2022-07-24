013a6735f830772871c72a933c7fbc7fb8676c38
Natesh Narain <nnaraindev@gmail.com>
2022-07-24 18:01:46 -0400

    Resolves #38. Script for plotting attitude.

--- icarus-cli/src/actions/log.rs
+++ icarus-cli/src/actions/log.rs
@@ -12,25 +12,28 @@ use clap::Parser;
 
 use serialport::SerialPort;
 
-use std::io;
-use std::sync::mpsc::channel;
+use std::{
+    io,
+    path::PathBuf,
+    sync::mpsc::channel,
+    time::{Duration, Instant}, str::FromStr,
+};
 
 use chrono::prelude::*;
 
-use csv::Writer;
 use serde::Serialize;
 
 
 #[derive(Parser, Debug)]
 pub struct Args {
-    /// CSV output
-    #[clap(value_parser)]
-    csv: String,
+    /// CSV output directory
+    #[clap(value_parser, default_value = "output")]
+    output_dir: String,
 }
 
 #[derive(Serialize, Debug)]
-struct ImuRow {
-    ts: String,
+struct SensorRow {
+    ts: f32,
     ax: f32,
     ay: f32,
     az: f32,
@@ -39,13 +42,37 @@ struct ImuRow {
     gz: f32,
 }
 
+// impl TryFrom<IcarusState> for SensorRow {
+//     fn try_from(value: IcarusState) -> Result<Self, Self::Error> {
+//         if let IcarusState::Sensors(sensors) =  {
+            
+//         }
+//         else {
+//             Err(())
+//         }
+//     }
+// }
+
+#[derive(Serialize, Debug)]
+struct AttitudeRow {
+    ts: f32,
+    pitch: f32,
+    roll: f32,
+    yaw: f32,
+}
+
 const READ_BUF_SIZE: usize = 1024;
 
 /// Run icarus logger
 /// See https://github.com/knurling-rs/defmt/blob/main/print/src/main.rs for defmt decoding
 pub fn run(mut ser: Box<dyn SerialPort>, args: Args) -> Result<()> {
-    let csv_output_path = args.csv;
-    let mut csv_writer = Writer::from_path(csv_output_path)?;
+    let out_dir = PathBuf::from_str(&args.output_dir)?;
+
+    let sensors_path = out_dir.join("sensors.csv");
+    let attitude_path = out_dir.join("attitude.csv");
+
+    let mut sensors_writer = csv::Writer::from_path(sensors_path)?;
+    let mut attitude_writer = csv::Writer::from_path(attitude_path)?;
 
     // Setup signal handler
     let (tx, rx) = channel();
@@ -56,6 +83,8 @@ pub fn run(mut ser: Box<dyn SerialPort>, args: Args) -> Result<()> {
     let mut filter_buf: Vec<u8> = vec![0; READ_BUF_SIZE];
 
     let mut cobs_buf: CobsAccumulator<256> = CobsAccumulator::new();
+
+    let start = Instant::now();
 
     loop {
         // Check if the user attempted to exit the program
@@ -70,13 +99,14 @@ pub fn run(mut ser: Box<dyn SerialPort>, args: Args) -> Result<()> {
                     break;
                 }
 
+                let now = Instant::now();
+
                 let filtered_len = filter_data(filter_buf.as_mut_slice(), &raw_buf[..n]);
 
                 let buf = &filter_buf[..filtered_len];
                 let mut window = buf;
 
                 'cobs: while !window.is_empty() {
-                    // println!("window: {}", window.len());
                     window = match cobs_buf.feed::<IcarusState>(window) {
                         FeedResult::Consumed => {
                             break 'cobs
@@ -88,7 +118,32 @@ pub fn run(mut ser: Box<dyn SerialPort>, args: Args) -> Result<()> {
                             new_wind
                         },
                         FeedResult::Success { data, remaining } => {
-                            println!("{:?}", data);
+                            // println!("{:?}", data);
+
+                            match data {
+                                IcarusState::Sensors(sensors) => {
+                                    let imu_row = SensorRow {
+                                        ts: now.duration_since(start).as_secs_f32(),
+                                        ax: sensors.accel.x,
+                                        ay: sensors.accel.y,
+                                        az: sensors.accel.z,
+                                        gx: sensors.gyro.x,
+                                        gy: sensors.gyro.y,
+                                        gz: sensors.gyro.z,
+                                    };
+                                    sensors_writer.serialize(imu_row)?;
+                                },
+                                IcarusState::EstimatedState(state) => {
+                                    let attitude_row = AttitudeRow {
+                                        ts: now.duration_since(start).as_secs_f32(),
+                                        pitch: state.attitude.pitch,
+                                        roll: state.attitude.roll,
+                                        yaw: state.attitude.yaw,
+                                    };
+                                    attitude_writer.serialize(attitude_row)?;
+                                },
+                                _ => {}
+                            }
 
                             remaining
                         }
@@ -102,6 +157,10 @@ pub fn run(mut ser: Box<dyn SerialPort>, args: Args) -> Result<()> {
 
     Ok(())
 }
+
+// fn write_to_output<W: io::Write, D: Serialize>(writer: W, data: D) {
+//     writer
+// }
 
 // Well this doesn't feel very Rust-y....
 fn filter_data(out: &mut [u8], recv: &[u8]) -> usize {
