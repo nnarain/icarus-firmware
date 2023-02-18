ed5cb3a305da137756f8dc0ee40d7a4082a59966
Natesh Narain <nnaraindev@gmail.com>
2023-02-17 21:49:52 -0500

    Resolve #50. Setup PWM output with minimum throttle

--- lib/rtrctl/RotorController.cpp
+++ lib/rtrctl/RotorController.cpp
@@ -4,10 +4,53 @@
 // @author Natesh Narain <nnaraindev@gmail.com>
 // @date Feb 04 2023
 //
-
+#include <Arduino.h>
 #include "RotorController.hpp"
 
-bool RotorController::begin()
+#define RTRCTL_CHNL1 0
+#define RTRCTL_CHNL2 1
+#define RTRCTL_CHNL3 2
+#define RTRCTL_CHNL4 3
+
+#define PWM_FREQ 200
+#define PWM_RESOLUTION 8
+
+// 200Hz -> 5ms
+// 8-bit resolution -> 255 steps
+// 5ms / 255 -> 0.0196078431372549 ms per step
+//
+// Max Throttle -> 2ms pulse width
+// 2ms / 0.0196078431372549 = 102
+//
+// Min Throttle -> 1ms pulse width
+// 1ms / 0.0196078431372549 = 51
+
+#define THROTTLE_MIN 51
+#define THROTTLE_MAX 102
+
+bool RotorController::begin(uint8_t rtr1, uint8_t rtr2, uint8_t rtr3, uint8_t rtr4)
 {
+    // Setup PWM channels
+    ledcSetup(RTRCTL_CHNL1, PWM_FREQ, PWM_RESOLUTION);
+    ledcSetup(RTRCTL_CHNL2, PWM_FREQ, PWM_RESOLUTION);
+    ledcSetup(RTRCTL_CHNL3, PWM_FREQ, PWM_RESOLUTION);
+    ledcSetup(RTRCTL_CHNL4, PWM_FREQ, PWM_RESOLUTION);
+
+    // Attach rotor pins
+    ledcAttachPin(rtr1, RTRCTL_CHNL1);
+    ledcAttachPin(rtr2, RTRCTL_CHNL2);
+    ledcAttachPin(rtr3, RTRCTL_CHNL3);
+    ledcAttachPin(rtr4, RTRCTL_CHNL4);
+
+    setThrottle(THROTTLE_MIN, THROTTLE_MIN, THROTTLE_MIN, THROTTLE_MIN);
+
     return true;
+}
+
+void RotorController::setThrottle(uint16_t t1, uint16_t t2, uint16_t t3, uint16_t t4)
+{
+    ledcWrite(RTRCTL_CHNL1, t1);
+    ledcWrite(RTRCTL_CHNL2, t2);
+    ledcWrite(RTRCTL_CHNL3, t3);
+    ledcWrite(RTRCTL_CHNL4, t4);
 }
--- lib/rtrctl/RotorController.hpp
+++ lib/rtrctl/RotorController.hpp
@@ -6,13 +6,21 @@
 //
 #pragma once
 
+#include <cstdint>
+
+/**
+ * @brief Throttle control for rotors
+ *
+ */
 class RotorController
 {
 public:
     RotorController() = default;
     ~RotorController() = default;
 
-    bool begin();
+    bool begin(uint8_t rtr1, uint8_t rtr2, uint8_t rtr3, uint8_t rtr4);
+
+    void setThrottle(uint16_t t1, uint16_t t2, uint16_t t3, uint16_t t4);
 
 private:
 };
