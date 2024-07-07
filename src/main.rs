3622a644971e519d477a7303f0a19a8cc5773532
Natesh Narain <nnaraindev@gmail.com>
2024-07-07 15:18:31 -0400

    uart tx async

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,12 @@ use embassy_stm32::{
     bind_interrupts,
     peripherals,
     usart::{self, UartTx, Config as UartConfig},
-    dma::NoDma,
 };
 
 use panic_halt as _;
 
 bind_interrupts!(struct Irqs {
-    USART1 => usart::BufferedInterruptHandler<peripherals::USART1>;
+    USART1 => usart::InterruptHandler<peripherals::USART1>;
 });
 
 #[embassy_executor::main]
@@ -22,9 +21,10 @@ async fn main(_spawner: Spawner) {
     // Use default configuration for UART: 115200 baud
     let usart_config = UartConfig::default();
 
-    let mut uart_tx = UartTx::new(dp.USART1, dp.PA9, NoDma, usart_config).unwrap();
+    let mut uart_tx = UartTx::new(dp.USART1, dp.PA9, dp.DMA2_CH7, usart_config).unwrap();
 
     loop {
-        uart_tx.blocking_write("Hello World\r\n".as_bytes()).unwrap();
+        // uart_tx.blocking_write("Hello World\r\n".as_bytes()).unwrap();
+        uart_tx.write("Hello Async World!\r\n".as_bytes()).await.unwrap();
     }
 }
