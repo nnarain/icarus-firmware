2f6dbee2dd36170f30e9f7b8fcd72381318a9142
Natesh Narain <nnaraindev@gmail.com>
2024-07-06 23:55:07 -0400

    Rust - New Beginning

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_stm32::{
+    bind_interrupts,
+    peripherals,
+    usart::{self, UartTx, Config as UartConfig},
+    dma::NoDma,
+};
+
+use panic_halt as _;
+
+bind_interrupts!(struct Irqs {
+    USART1 => usart::BufferedInterruptHandler<peripherals::USART1>;
+});
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let dp = embassy_stm32::init(Default::default());
+
+    // Use default configuration for UART: 115200 baud
+    let usart_config = UartConfig::default();
+
+    let mut uart_tx = UartTx::new(dp.USART1, dp.PA9, NoDma, usart_config).unwrap();
+
+    loop {
+        uart_tx.blocking_write("Hello World\r\n".as_bytes()).unwrap();
+    }
+}
